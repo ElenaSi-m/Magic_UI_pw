c85306e116d4d9422540963d8eaa3a0b74cc384a
ElenaSi-m <sinitsinaelena24@gmail.com>
2025-07-21 22:27:30 +0700

    Update test_eco_friendly_page.py

--- tests/test_eco_friendly_page.py
+++ tests/test_eco_friendly_page.py
@@ -1,13 +1,16 @@
+import pytest
+
+@pytest.mark.smoke
 def test_header_title(eco_friendly_page):
     eco_friendly_page.open_page()
     eco_friendly_page.check_page_header_title_is("Eco Friendly")
 
-
+@pytest.mark.regression
 def test_check_add_cart_button_first_product(eco_friendly_page):
     eco_friendly_page.open_page()
     eco_friendly_page.check_first_product_button()
 
-
+@pytest.mark.extended
 def test_products_are_displayed(eco_friendly_page):
     eco_friendly_page.open_page()
     eco_friendly_page.check_if_have_products()
